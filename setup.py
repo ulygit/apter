cc9360b5cd8c7aac54f65e3e5df9d8b906237bc6
ulygit <ulygit@bfjournal.com>
2020-03-05 22:33:12 -0500

    Version 0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ APP_NAME = 'apter'
 # noinspection SpellCheckingInspection
 setup(
     name=APP_NAME,
-    version='0.5',
+    version='0.6',
     description='A sweet project configuration package',
     url='https://github.com/ulygit/apter',
     author='U. Melendez',
