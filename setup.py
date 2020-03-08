7140fe683ecb006a6de042bb6105d61e2b472d91
ulygit <ulygit@bfjournal.com>
2020-03-07 21:20:54 -0500

    Bump version to 0.8.dev1 after release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ APP_NAME = 'apter'
 # noinspection SpellCheckingInspection
 setup(
     name=APP_NAME,
-    version='0.7',
+    version='0.8.dev1',
     description='A sweet project configuration package',
     url='https://github.com/ulygit/apter',
     author='U. Melendez',
